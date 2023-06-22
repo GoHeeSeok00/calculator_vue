ad25b8c436046cd8d1ae43bf22d3a7d8279cd835
GoHeeSeok00 <weeds1590@gmail.com>
2023-06-22 19:25:43 +0900

    * 계산기 ui 세팅

--- quasar-project/src/components/Calculator.vue
+++ quasar-project/src/components/Calculator.vue
@@ -1,14 +1,58 @@
 <template>
-  <div>안녕</div>
+  <div>
+    <div>계산기</div>
+    <div>
+      <q-input rounded outlined v-model="calResult" />
+    </div>
+    <div>
+      <q-btn class="glossy cal-btn" rounded color="deep-orange" label="C" />
+      <q-btn class="glossy cal-btn" rounded color="deep-orange" label="<-" />
+      <q-btn class="glossy cal-btn" rounded color="teal" label="*" />
+    </div>
+    <div>
+      <q-btn class="glossy cal-btn" rounded color="teal" label="+" />
+      <q-btn class="glossy cal-btn" rounded color="teal" label="-" />
+      <q-btn class="glossy cal-btn" rounded color="teal" label="%" />
+    </div>
+    <div>
+      <q-btn class="glossy cal-btn" rounded color="primary" label="7" />
+      <q-btn class="glossy cal-btn" rounded color="primary" label="8" />
+      <q-btn class="glossy cal-btn" rounded color="primary" label="9" />
+    </div>
+    <div>
+      <q-btn class="glossy cal-btn" rounded color="primary" label="4" />
+      <q-btn class="glossy cal-btn" rounded color="primary" label="5" />
+      <q-btn class="glossy cal-btn" rounded color="primary" label="6" />
+    </div>
+    <div>
+      <q-btn class="glossy cal-btn" rounded color="primary" label="1" />
+      <q-btn class="glossy cal-btn" rounded color="primary" label="2" />
+      <q-btn class="glossy cal-btn" rounded color="primary" label="3" />
+    </div>
+    <div>
+      <q-btn class="glossy cal-btn" rounded color="primary" label="0" />
+      <q-btn class="glossy cal-btn" rounded color="primary" label="." />
+      <q-btn class="glossy cal-btn" rounded color="teal" label="=" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'VictorCalculator',
   setup() {
-    return {}
+    const calResult = ref('')
+    return { calResult }
   }
 })
 </script>
+
+<style scoped lang="scss">
+.cal-btn {
+  margin: 0.5rem;
+  width: 5rem;
+  height: 1rem;
+}
+</style>
